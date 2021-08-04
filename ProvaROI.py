f342f525a5dd509cf067a4b375725d987c7af8c9
marcoesu <marco.esu@outlook.it>
2021-08-04 21:50:52 +0200

    Update ProvaROI.py

--- ProvaROI.py
+++ ProvaROI.py
@@ -28,17 +28,36 @@ for pino in scansione:
             image = cv.imread(f1)
             altezza, larghezza, colori = image.shape
             dim_scaled = (int(larghezza/altezza*800),800)
+            img_scaled = cv.resize(image, dim_scaled, interpolation = cv.INTER_AREA)
             #img_to_zero = cv.cvtColor(image, cv.THRESH_TOZERO)     #la radice assume una colorazione tendente al celeste
-            img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)          #Utilizzando invece TRUNC l'immagine assume una colorazione leggermente più scura di THRESH_TO_ZERO
-            lower_range = np.array([20,15,60])                      #Utilizzando la conversione in HSV in combinazione con la maschera otteniamo buona parte delle radici
-            upper_range = np.array([120,70,250])                    #I limiti della maschera
-            img_scaled = cv.resize(img_hsv, dim_scaled, interpolation = cv.INTER_AREA)     #L'immagine viene scalata per essere visibile completamente a schermo
+            img_hsv= cv.cvtColor(img_scaled, cv.COLOR_BGR2HSV)
+            
+            #lower_range = np.array([20,15,60])                      #Utilizzando la conversione in HSV in combinazione con la maschera otteniamo buona parte delle radici
+            #upper_range = np.array([120,70,250])                    #I limiti della maschera
+            #lower_range = np.array([20, 20, 50], dtype=np.uint8)
+            #upper_range = np.array([50,255,255], dtype=np.uint8)                    #I limiti della maschera
+            
+            lower_range = np.array([30, 80, 30], dtype=np.uint8)
+            upper_range = np.array([150,255,150], dtype=np.uint8) 
+            
+            mask = cv.inRange(img_hsv, lower_range, upper_range)
+            #img_gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)          #Utilizzando invece TRUNC l'immagine assume una colorazione leggermente più scura di THRESH_TO_ZERO
+
+            #L'immagine viene scalata per essere visibile completamente a schermo
             #ret,thresh1 = cv.threshold(img_scaled,110,250,cv.THRESH_BINARY)
-            mask = cv.inRange(img_scaled, lower_range, upper_range)
+            #imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+            ret, thresh = cv.threshold(mask, 127, 255, 0)
+            contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+            #mask = cv.inRange(img_scaled, lower_range, upper_range)
             #mask_scaled = cv.resize(mask, dim_scaled, interpolation = cv.INTER_AREA)
-            cv.imshow('hsv', mask)
-            cv.imshow('to zero', img_scaled)
+            #cv.imshow('hsv', mask)
+            cv.drawContours(img_hsv, contours, -1, (0,255,0), 3)
+            cv.imshow('prova', mask)
+            cv.imshow('to zero', img_hsv)
             #cv.imwrite(str(pippo +'_HSV.jpg'), img_trunc)
+
+
+        # https://docs.opencv.org/master/dd/d49/tutorial_py_contour_features.html
 
             cv.waitKey(3000)
 
