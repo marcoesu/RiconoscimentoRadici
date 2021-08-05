652d132b204cacaf8e8f38345b17d2d4955abb1b
marcoesu <marco.esu@outlook.it>
2021-08-05 14:12:29 +0200

    Update ProvaROI.py

--- ProvaROI.py
+++ ProvaROI.py
@@ -13,21 +13,20 @@ scanpath = os.scandir()
 print('Files and Directories in ' + path)
 scansione = os.scandir()
 print(scansione)
-for pino in scansione:
-    if pino.is_dir()== True:
-        #print(pino.name)
-        subpath = str(path + r'/'+ pino.name)
+for sottocartella in scansione:
+    if sottocartella.is_dir()== True:
+        subpath = str(path + r'/'+ sottocartella.name)
         os.chdir(subpath)
         scansubdir = os.scandir()
         data_path = os.path.join(subpath,'*.jpg')
         files = glob.glob(data_path) #converte data path in un output Unix-like (ls) (*.jpg -> lista di elementi con estensione jpg)
         #print(files)
-# Ogni soggetto è identificato dal QR, che viene decodificato ed utilizzato per rinominare le rispettive sottocartelle. 
+        #Ciclo per scorrere tutte le immagini delle sottocartelle 
         for f1 in files:
-            pippo = os.path.basename(f1)
+            nomefile = os.path.basename(f1)
             image = cv.imread(f1)
-            altezza, larghezza, colori = image.shape
-            dim_scaled = (int(larghezza/altezza*800),800)
+            altezza, larghezza, colori = image.shape    
+            dim_scaled = (int(larghezza/altezza*800),800)           # Immagine rimpicciolita per comodità. Le immagini originali sono 6000x4000.
             img_scaled = cv.resize(image, dim_scaled, interpolation = cv.INTER_AREA)
             #img_to_zero = cv.cvtColor(image, cv.THRESH_TOZERO)     #la radice assume una colorazione tendente al celeste
             img_hsv= cv.cvtColor(img_scaled, cv.COLOR_BGR2HSV)
@@ -35,12 +34,12 @@ for pino in scansione:
             #lower_range = np.array([20,15,60])                      #Utilizzando la conversione in HSV in combinazione con la maschera otteniamo buona parte delle radici
             #upper_range = np.array([120,70,250])                    #I limiti della maschera
             #lower_range = np.array([20, 20, 50], dtype=np.uint8)
-            #upper_range = np.array([50,255,255], dtype=np.uint8)                    #I limiti della maschera
+            #upper_range = np.array([50,255,255], dtype=np.uint8)                    #I limiti della maschera, colore rosso (HSV)
             
-            lower_range = np.array([30, 80, 30], dtype=np.uint8)
-            upper_range = np.array([150,255,150], dtype=np.uint8) 
+            lower_green = np.array([30, 80, 30], dtype=np.uint8)            #range per il colore verde
+            green_green = np.array([150,255,150], dtype=np.uint8)           #per evidenziare il cartoncino blu (nell'immagine convertita in hsv è verde)
             
-            mask = cv.inRange(img_hsv, lower_range, upper_range)
+            mask = cv.inRange(img_hsv, lower_green, upper_green)
             #img_gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)          #Utilizzando invece TRUNC l'immagine assume una colorazione leggermente più scura di THRESH_TO_ZERO
 
             #L'immagine viene scalata per essere visibile completamente a schermo
