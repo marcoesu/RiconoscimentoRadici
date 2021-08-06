58b1f650799fe925a593590c9b69fe45e27c6908
marcoesu <marco.esu@outlook.it>
2021-08-06 11:47:41 +0200

    Update identificazioneQR.py

--- identificazioneQR.py
+++ identificazioneQR.py
@@ -1,29 +1,28 @@
-from genericpath import isfile
 import cv2 as cv
 #import numpy as np
-from pyzbar.pyzbar import decode #decodifica del QR
+from pyzbar.pyzbar import decode #decodifica del QR     (pip install pyzbar)
 import os # utilizzata per effettuare operazioni sulle cartelle
 import glob
 import shutil #permette di effettuare operazioni su file
-
 import requests #pip install requests
 import zipfile
 
-def PresenzaCampioni(path,url):
+def PresenzaCampioni(path,url):     # Funzione adibita al controllo della presenza del file contenente le immagini campione
 
     if(os.path.exists(str(path + r'/FotoCampione.zip'))):
         print("Campioni presenti")
     else:
         try:
-            FotoCampione = requests.get(url)
-            with open('FotoCampione.zip', 'wb') as local_file: #Salvataggio su disco, nella cartella path, del file .zip
+            print("Download dei campioni in corso...")
+            FotoCampione = requests.get(url)                   
+            with open('FotoCampione.zip', 'wb') as local_file: # Salvataggio su disco, nella cartella path, del file .zip
                 local_file.write(FotoCampione.content)
-            print("Campioni scaricati")
+            print("Campioni scaricati.")
         except:
-            print("Impossibile scaricare i campioni") 
+            print("Impossibile scaricare i campioni.")         # Errore di download
 
 
-def EstrazioneCampioni():
+def EstrazioneCampioni():   # Funzione che si occupa dell'estrazione delle immagini campione dal file .zip
 
     zip = "FotoCampione.zip"
     try:
@@ -34,13 +33,13 @@ def EstrazioneCampioni():
         print("File non valido.")
 
 
-path = os.path.abspath(os.path.dirname(__file__)) #salva nella variabile path il percorso globale della cartella in cui si trova il file .py in esecuzione
-os.chdir(path)  #cambio della cartella attuale nella cartella in cui si trova il file .py
+path = os.path.abspath(os.path.dirname(__file__)) #Salva nella variabile path il percorso globale della cartella in cui si trova il file .py in esecuzione
+os.chdir(path)  # Cambio della cartella attuale nella cartella in cui si trova il file .py
 url ='https://www.dropbox.com/s/2lourrj1s3wck3q/FotoCampione.zip?dl=1' # File .zip contenente le immagini campione, salvato su DropBox 
 
-PresenzaCampioni(path,url) #controllo della presenza del file FotoCampione.zip nel path
+PresenzaCampioni(path,url) # Controllo della presenza del file FotoCampione.zip nel path
 
-EstrazioneCampioni() #estrazione dei campioni dal file FotoCampione.zip
+EstrazioneCampioni() # Estrazione dei campioni dal file FotoCampione.zip
 
 data_path = os.path.join(path,'*.jpg') #lista di tutti gli elementi di estensione jpg nella cartella path
 files = glob.glob(data_path) #converte data path in un output Unix-like (ls) (*.jpg -> lista di elementi con estensione jpg)
@@ -56,4 +55,6 @@ for f1 in files: # ciclo che scorre le immagini nella cartella path
         #cv.waitKey(0)
         if not os.path.exists(path+r'/'+codid):     # controlla che esista la sottocartella dedicata al soggetto in analisi
             os.makedirs(path+r'/'+codid)            #crea la cartella dedicata
+            print(str('Cartella '+codid+' creata.'))
         shutil.move(f1,str(os.path.dirname(f1) + r'/' + codid + r'/' + os.path.basename(f1)))   #sposta il file jpg dalla cartella path alla sottocartella del rispettivo soggetto
+print("Processo terminato.")        
