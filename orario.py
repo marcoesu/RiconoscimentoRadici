83a657f9a2dab0df2446cdfa8d01b23c9be667b9
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-09-20 17:13:37 +0200

    aggiunta per rinominare l'immagine con l'orario

--- orario.py
+++ orario.py
@@ -27,23 +27,9 @@ for sottocartella in scansione: #ciclo per scansionare le sottocartelle di path
             image = cv.imread(f1)   #lettura dell'immagine dal disco
             with open(nomefile, 'rb') as fh:
                 tags = exifread.process_file(fh, stop_tag="EXIF DateTimeOriginal")
-                dateTaken = tags["EXIF DateTimeOriginal"]
-                print(dateTaken)
-            '''
-            im = Image.open(nomefile)
-            im.load
-            print(im.info['meta_to_read'])
-            
-            tags = ["File Name", "CreateDate"]
-            log_file = f1
-            with exiftool.ExifTool() as et:
-                metadata = et.get_tags_batch(tags,nomefile)
-            f = open(log_file, "w")
-            f.write(str(metadata))
-            f.close()    
-            with open(log_file, 'w') as fd:
-                json.dump(metadata, log_file)
-            with open(log_file, 'r') as fd:
-                metadata = json.load(fd)    
-            for imageinfo in metadata:
-                print('CreateDate:', imageinfo.get('EXIF:CreateDate', 'unknown'))'''
+                dt = str(tags['EXIF DateTimeOriginal']) 
+                #dt = tags.get('DateTimeOriginal', 'Not available')
+                day, dtime = dt.split(" ", 1)
+                hour, minute, second = dtime.split(":", 2)
+                nomefile,ext = os.path.splitext(nomefile)    
+                cv.imwrite(str(nomefile+'_'+dt+'.jpg'), image)
