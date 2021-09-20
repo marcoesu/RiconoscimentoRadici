cdc7906ebb4833f4ef516462b01e2098d873256e
marcoesu <marco.esu@outlook.it>
2021-09-20 14:17:40 +0200

    Utilizzo dei nuovi campioni

--- identificazioneQR.py
+++ identificazioneQR.py
@@ -32,29 +32,74 @@ def EstrazioneCampioni():   # Funzione che si occupa dell'estrazione delle immag
     except:
         print("File non valido.")
 
+def Archiviazione(path,file,codice): #Spostamento file campione nella sottocartella dedicata
+    if not os.path.exists(path+r'/'+codice):     # controlla che esista la sottocartella dedicata al soggetto in analisi
+            os.makedirs(path+r'/'+codice)            #crea la cartella dedicata
+            print(str('Cartella '+codice +' creata.'))
+    shutil.move(file,str(os.path.dirname(file) + r'/' + codice + r'/' + os.path.basename(file)))
+
 
 path = os.path.abspath(os.path.dirname(__file__)) #Salva nella variabile path il percorso globale della cartella in cui si trova il file .py in esecuzione
 os.chdir(path)  # Cambio della cartella attuale nella cartella in cui si trova il file .py
-url ='https://www.dropbox.com/s/2lourrj1s3wck3q/FotoCampione.zip?dl=1' # File .zip contenente le immagini campione, salvato su DropBox 
+url ='https://www.dropbox.com/s/opu3e8kpoujio1t/FotoCampione.zip?dl=1' # File .zip contenente le immagini campione, salvato su DropBox 
 
 PresenzaCampioni(path,url) # Controllo della presenza del file FotoCampione.zip nel path
 
 EstrazioneCampioni() # Estrazione dei campioni dal file FotoCampione.zip
 
-data_path = os.path.join(path,'*.jpg') #lista di tutti gli elementi di estensione jpg nella cartella path
+data_path = os.path.join(path,'*[0-9].jpg') #lista di tutti gli elementi di estensione jpg nella cartella path
 files = glob.glob(data_path) #converte data path in un output Unix-like (ls) (*.jpg -> lista di elementi con estensione jpg)
 
 # Ogni soggetto è identificato dal QR, che viene decodificato ed utilizzato per rinominare le rispettive sottocartelle. 
 for f1 in files: # ciclo che scorre le immagini nella cartella path
+    
     image = cv.imread(f1)   #lettura dell'immagine
-    altezza, larghezza, colori = image.shape #salva le dimensioni dell'immagine (espresse come una tupla) in dim. quindi altezza = dim[1], lunghezza = dim[2]
-    zonaqr=image[(int(altezza/4)):(int(altezza/2)),int((larghezza/10)):int((larghezza/3.5))]    # sezione dell'immagine analizzata contenente il QR identificativo
-    for qr in decode(zonaqr):   #ciclo for utilizzato per decodificare il QR di ogni immagine
-        codid=qr.data.decode('utf-8')    #codice identificativo estratto dal QR
+    altezza, larghezza = image.shape[:2] #salva le dimensioni dell'immagine (espresse come una tupla) in dim. quindi altezza = dim[1], lunghezza = dim[2]
+    #zonaqr=image[(int(altezza/4)):(int(altezza/2)),int((larghezza/10)):int((larghezza/3.5))]    # sezione dell'immagine analizzata contenente il QR identificativo
+    zonaqrsx=image[(int(altezza/6)):(int(altezza*0.3)),int((larghezza/10)):int((larghezza/5))]    # sezione dell'immagine analizzata contenente il QR identificativo
+    zonaqrdx=image[(int(altezza*0.15)):(int(altezza*0.3)),int((larghezza*0.8)):int((larghezza*0.9))]
+    #zonaqr=image[(int(altezza*0.2)):(int(altezza*0.3)),int((larghezza*0.8)):int((larghezza*0.9))]
+    #zonaqr=image[int(1200):int(1420),int(490):int(710)]
+    #zonaqr2=image[3300:3560,1200:1460]
+    '''for qrsx in decode(zonaqrsx):   #ciclo for utilizzato per decodificare il QR di ogni immagine
+        print(str(os.path.basename(f1)))
+        codid=qrsx.data.decode('utf-8')    #codice identificativo estratto dal QR
+        print(codid +"ciao")
+        if codid: Archiviazione(path,f1,codid)
+        else:
+            print("QR sinistro non riconosciuto")
+            cv.imwrite(str(os.path.basename(f1)+'_qrsx.jpg'), zonaqrsx)
+            for qrdx in decode(zonaqrdx):   #ciclo for utilizzato per decodificare il QR di ogni immagine
+                codid=qrdx.data.decode('utf-8')    #codice identificativo estratto dal QR
+                print(codid)
+                if codid: Archiviazione(path,f1,codid)
+                else: cv.imwrite(str(os.path.basename(f1)+'qrdx.jpg'), zonaqrdx)'''
+    for qrsx in decode(zonaqrsx):  #ciclo for utilizzato per decodificare il QR di ogni immagine
+        codid=qrsx.data.decode('utf-8')    #codice identificativo estratto dal QR
+        Archiviazione(path,f1,codid)
+        print("Esecuzione decod. su QR a sinistra per "+ str(os.path.basename(f1)))
+    for qrdx in decode(zonaqrdx):   #ciclo for utilizzato per decodificare il QR di ogni immagine
+        codid=qrdx.data.decode('utf-8')    #codice identificativo estratto dal QR
+        Archiviazione(path,f1,codid)
+        print("Esecuzione decod. su QR a destra per "+ str(os.path.basename(f1)))
+    cv.imwrite(str(os.path.basename(f1)+'qrsx.jpg'), zonaqrsx)
+    cv.imwrite(str(os.path.basename(f1)+'qrdx.jpg'), zonaqrdx)
+
+
+
+
+
+
+'''
+    
+        if not codid:
+            print("ciao")
+            for qr2 in decode(zonaqr2):
+                codid=qr.data.decode('utf-8')
         #cv.imshow(str(codid),zonaqr)
         #cv.waitKey(0)
         if not os.path.exists(path+r'/'+codid):     # controlla che esista la sottocartella dedicata al soggetto in analisi
             os.makedirs(path+r'/'+codid)            #crea la cartella dedicata
             print(str('Cartella '+codid+' creata.'))
-        shutil.move(f1,str(os.path.dirname(f1) + r'/' + codid + r'/' + os.path.basename(f1)))   #sposta il file jpg dalla cartella path alla sottocartella del rispettivo soggetto
-print("Processo terminato.")        
+        shutil.move(f1,str(os.path.dirname(f1) + r'/' + codid + r'/' + os.path.basename(f1)))'''   #sposta il file jpg dalla cartella path alla sottocartella del rispettivo soggetto'''
+print("Processo terminato.")      
--- scacchi.py
+++ scacchi.py
@@ -11,14 +11,29 @@ lower_green = np.array([0, 0, 0])
 upper_green = np.array([150,100,100])
 cv.imwrite(r'C:\Users\esu7z\Desktop\GitHub\IdentificazioneQR\ciao.jpg', hsv)                       
 mask = cv.inRange(hsv, lower_green, upper_green)
+contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE) #Ricerca dei contorni utlizzando le informazioni ottenute attraverso il tresholding
+c = max(contours, key=cv.contourArea)   #Ricerca del più grande contorno nell'immagine utilizzando come parametro di giudizio l'area del contorno
+x, y, w, h = cv.boundingRect(c)
 mask=cv.bitwise_not(mask)
+scacchiera=mask[y:y+h,x:x+w]
+#scacchiera=img[y:y+h,x:x+w,:]
+# mask=cv.bitwise_not(mask)
+
 gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-pino = cv.bitwise_and(gray, gray, mask= mask)
-ret, corners = cv.findCirclesGrid(pino, (2,1), None)
-cv.imwrite(r'C:\Users\esu7z\Desktop\GitHub\IdentificazioneQR\ciaoo.jpg', pino)
+pino = cv.bitwise_or(gray, mask)
+#pino= cv.morphologyEx(pino, cv.MORPH_OPEN, (1,1))
+#pino= cv.morphologyEx(pino, cv.MORPH_OPEN, (1,1))
+#pino = cv.dilate(pino,(1,1),iterations = 5)
+#pino = cv.bilateralFilter(pino,9,75,75)
+#params = cv.SimpleBlobDetector_Params()
+#params.maxArea = 100000
+#detector = cv.SimpleBlobDetector_create(params)
+ret, corners = cv.findCirclesGrid(scacchiera, (1,30), cv.CALIB_CB_ASYMMETRIC_GRID + cv.CALIB_CB_CLUSTERING) #,  blobDetector=detector)
+print(corners)
+cv.imwrite(r'C:\Users\esu7z\Desktop\GitHub\IdentificazioneQR\ciaoo.jpg', scacchiera)
 #cv.waitKey(2000)
 if(ret==True):
     corners2 = cv.cornerSubPix(pino,corners, (1,1), (-1,-1), (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001))
-    cv.drawChessboardCorners(img, (2,1), corners, ret)
+    cv.drawChessboardCorners(img, (2,1), corners2, ret)
     print("ciao")
     cv.imwrite(str(r'C:\Users\esu7z\Desktop\GitHub\IdentificazioneQR\b.jpg'+'exit.jpg'), img)
