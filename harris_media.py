628d44b5fa4c6d2c3f717c0571e1df55dd5a72f9
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-09-30 18:44:41 +0200

    punti medi

--- harris_media.py
+++ harris_media.py
@@ -5,6 +5,7 @@ import os
 import matplotlib.pyplot as plt
 import math
 from numpy.core.fromnumeric import ravel
+from numpy.core.numeric import count_nonzero
 from numpy.lib.function_base import average
 from sklearn.cluster import DBSCAN #pip install scikit-learn
 file = open("prova.txt","w")
@@ -17,7 +18,7 @@ img = cv.imread(r'prova_dbscan.jpg',0) #lettura immagine
 harris = cv.cornerHarris(img,2,3,0.04) #applicazione dell'algoritmo di harris
 
 #result is dilated for marking the corners, not important
-harris = cv.dilate(harris,None) # ingrandisce il corner
+#harris = cv.dilate(harris,None) # ingrandisce il corner
 #dst = dst.astype(np.uint8)
 
 img = cv.cvtColor(img, cv.COLOR_GRAY2BGR) # conversione da bianco e nero a RGB
@@ -28,16 +29,58 @@ img[harris>0.02*harris.max()]=[0,0,255] #disegna i cerchi rossi sullo scheletro
 altezza, larghezza = harris.shape[:2] #dimensioni dell'immagine ottenuta con harris
 nero = np.zeros((altezza,larghezza,1)) #crea un'immagine completamente nera
 nero[harris>0.02*harris.max()]=[255] #disegna i punti di interesse trovati con harris su un'immagine nera
-#bianco = nero.astype(np.uint8)
-'''print(nero)
-file.write(str(nero))
-file.write('------------------------------------------------')
-'''
+
 nero = nero.astype(np.uint8) #i punti riportati da harris sono in subpixel,
                             # andiamo quindi a convertire l'immagine in un array di interi
                             # approssimando la posizione dei punti ottenuti con l'algoritmo di harris
                             # in pixel
 print('----------------------')
+
+cv.imwrite('nero.png', nero)
+
+raggio=5
+#definizione zona
+#grandezza=np.count
+#x= np.ndarray(int(np))
+
+riga = raggio
+
+while (riga < altezza-raggio) : 
+    colonna = raggio
+    while (colonna < larghezza-raggio) :
+        if(nero[riga][colonna] == 255):
+            area = nero[int(riga - raggio):int(riga+raggio),int(colonna-raggio):int(colonna+raggio)]
+            print(area.shape)
+            n_pixel = np.count_nonzero(area)
+            if (n_pixel>1):
+                media_punti_x=np.ndarray(n_pixel)
+                media_punti_y=np.ndarray(n_pixel)
+                riga_area = 0
+                while (riga_area<area.shape[0]):
+                    colonna_area = 0
+                    pixel = 0
+                    while (colonna_area<area.shape[1] and pixel<n_pixel):
+                        if(area[riga_area,colonna_area]==[255]):
+                            media_punti_y[pixel]=riga_area
+                            media_punti_x[pixel]=colonna_area
+                            pixel+=1
+                        colonna_area+=1
+                    riga_area+=1
+                media_x=(sum(media_punti_x)/n_pixel).astype(np.uint8)
+                media_y=(sum(media_punti_y)/n_pixel).astype(np.uint8)
+                print(media_y)
+                print(media_x)
+                area[0:9,0:9]=[0]
+                print(area)
+                area[(media_y),int(media_x)]=[255]
+                nero[riga - raggio:riga+raggio,colonna-raggio:colonna+raggio]=area
+            
+        colonna = colonna+1
+    riga = riga+1 
+
+cv.imwrite("arturo.png",nero)
+
+'''
 print(nero)
 file.write(str(nero))
 
@@ -60,9 +103,6 @@ while (riga < altezza) :
     riga = riga+1        
 
 x = x.reshape(grandezza,2) #effettuiamo un reshape per accorpare le coordinate x e y in un singolo elemento dell'array (matrice mx2)
-#print(x)
-
-#print('------------------')
 
 cv.imwrite('harris.jpg', img)
 
@@ -71,24 +111,10 @@ cv.imwrite('nero.png', nero)
 db = DBSCAN(eps=20, min_samples=3).fit(x) #utilizziamo l'array x con dbscan per trovare i cluster di punti
 
 labels = db.labels_ #array contenente le etichette(cluster di appartenenza) di ogni punto dell'array x
-#print(labels)
-np.set_printoptions(threshold=labels.size)
-#file = open("prova.txt","w")
-#file.write(str(labels))
-cont = 0
 
-'''
-for l in labels:    
-    if (l != -1):
-        if (l == cont) : #scorrimento
-            file.write('ciao')
-        elif(l != cont) :
-            file.write(str(l))
-            file.write('#######')
-            cont += 1
-            file.write(str(cont))
-            file.write('-------')   
-print(cont)'''    
+np.set_printoptions(threshold=labels.size)
+
+cont = 0 
 
 
 cv.imwrite('output.png', db.labels_)
@@ -142,9 +168,6 @@ while (contatore < labels.size) and n_cluster < grandezza_p_medi:     #while (co
 
         media_punti[copia_corners] = x[int(puntatore+copia_corners)]
         
-        '''#andiamo a salvare il punto medio
-        p_medi[n_cluster] = average(media_punti)'''
-        
         copia_corners +=1
 
     #definizione degli array che conterranno le coordinate x e y dei punti appartenenti al cluster analizzato
@@ -163,14 +186,8 @@ while (contatore < labels.size) and n_cluster < grandezza_p_medi:     #while (co
         c_xy=c_xy+1
 
     #variabili contententi rispettivamente la media delle ascisse e delle ordinate dei punti (convertiti in int)    
-    '''media_x=(np.average(media_punti_x))#.astype(np.uint8)
-    media_y=(np.average(media_punti_y))#.astype(np.uint8)'''
-
-    '''    media_x=(sum(media_punti_x)/n_elementi_cluster)#.astype(np.uint8)
-    media_y=(sum(media_punti_y)/n_elementi_cluster)#.astype(np.uint8)
-
-    print('media_x '+ str(media_x))
-    print('media_y '+ str(media_y))'''
+    media_x=(np.average(media_punti_x))#.astype(np.uint8)
+    media_y=(np.average(media_punti_y))#.astype(np.uint8)
 
 
     media_x=(sum(media_punti_x)/n_elementi_cluster).astype(np.uint32)
@@ -203,102 +220,4 @@ cv.imwrite('dbscan.png',nero)
 #print(img.shape)
 print(nero.shape)
 #print(harris.shape)
-print(dbscan.shape)
-
-'''harris=cv.imread('harris.png',1)
-dbscan=cv.imread('dbscan.png')
-print(harris.shape)
-print(dbscan.shape)
-print(img.shape)
-#prova = cv.bitwise_or(dbscan,harris,mask = img)
-prova = cv.addWeighted(img,0.7,dbscan,0.3,0)
-
-cv.imwrite('aaaaaaaaaaaaaaaaaaaaaaaprova.png',prova)'''
-
-#p_medi[n_cluster]
-
-'''p_medi = p_medi.astype(np.uint8)
-print(p_medi)'''
-
-'''while (contatore < grandezza and n_cluster < grandezza_p_medi):
-    n_elementi_cluster = 0
-    #while (n_cluster <= grandezza_p_medi): #contare quanti elementi ci sono nel cluster
-    while(db.labels_[contatore] == n_cluster):
-        n_elementi_cluster += 1
-        contatore += 1      
-    #elif((db.labels_[contatore] == (n_cluster+1)) or db.labels_[contatore] == -1):
-    #print(str(n_elementi_cluster) + ' elementi nel cluster' + str(n_cluster))
-    copia_corners = 0
-    #media_punti = np.ndarray(n_elementi_cluster*2).reshape(n_elementi_cluster,2)
-    media_punti = np.ndarray(n_elementi_cluster*2).reshape(n_elementi_cluster,2)
-    #print('------------')
-    #print(media_punti.shape)
-    #print(x.shape)
-    #print('-------------')
-    #print(n_elementi_cluster)
-    while(copia_corners < (n_elementi_cluster)):
-        print('ciao')
-        media_punti[copia_corners] = x[int(puntatore+copia_corners)]
-        print(x[int(puntatore+copia_corners)])
-        print('media punti' + str(media_punti))
-        
-        p_medi[n_cluster] = average(media_punti)
-        print(p_medi[n_cluster])
-        copia_corners +=1
-    
-    media_punti_x = np.ndarray(n_elementi_cluster)
-    media_punti_y = np.ndarray(n_elementi_cluster)
-    media_punti = media_punti.ravel()
-    c=0
-    c_xy=0
-    dim_media_punti = n_elementi_cluster
-    while c<n_elementi_cluster:
-        media_punti_x[c_xy] = media_punti[c]
-        c=c+1
-        media_punti_y[c_xy] = media_punti[c]
-        c=c+1
-        c_xy=c_xy+1
-    media_x=(average(media_punti_x)).astype(np.uint8)
-    media_y=(average(media_punti_y)).astype(np.uint8)
-#   tmp=np.ndarray([[media_x][media_y]]).reshape(2,2) #errore
-    print(p_medi.shape)
-    p_medi[n_cluster*2-1]=media_y #=[int(average(media_punti_x))int(average(media_punti_x))]
-    p_medi[n_cluster*2]=media_x
-
-    n_cluster += 1
-    contatore += 1
-    puntatore = contatore
-
-
-
-c = 0
-
-while(c < p_medi.size):
-    dbscan[int(p_medi[c+1])][int(p_medi[c])]= [0,255,0]
-    c = c+2
-
-cv.imwrite('dbscan.png',dbscan)
-
-#p_medi[n_cluster]
-
-    
-
-p_medi = p_medi.astype(np.uint8)
-print(p_medi)'''
-
-
-'''
-riga = 0
-contatore = 0
-
-while (riga < altezza) :
-    colonna = 0
-    while (colonna < larghezza) :
-        if(nero[riga][colonna] == 255) :
-            x[contatore] = colonna
-            x[contatore+1] = riga
-            contatore = contatore+2
-        colonna = colonna+1
-    riga = riga+1'''
-
-#cv.imwrite('bianco.png', bianco)
+print(dbscan.shape)'''
